c9cf1e57a8ae3d5e279507411a7c64397c4f6af7
Gabriela Teixeira <gabriela.teixeira92@gmail.com>
2023-08-04 20:52:19 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/gabicteixeira.png" />
+    <Titulo fontSize={20}>Gabriela Teixeira</Titulo>
+  </aside>
+)
+
+export default Sidebar
