9293daaf412de551fa7949868b1ff80181a9f5a5
Gabriela Teixeira <gabriela.teixeira92@gmail.com>
2023-08-06 23:07:33 -0300

    Estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/gabicteixeira.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/gabicteixeira.png" />
-    <Titulo fontSize={20}>Gabriela Teixeira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gabriela Teixeira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        gabicteixeira
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Font-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
